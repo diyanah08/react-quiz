330646392d0f0c3eb507678a8f0cb8bc4287270e
N Diyanah <diyanah08@gmail.com>
2019-12-30 19:18:10 +0800

    added welcome message

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -3,6 +3,15 @@ import React from "react"
 export default function Welcome()
 {
     return (
-        <h1>Welcome!</h1>
+        <>
+            <h1>Welcome!</h1>
+            <h2>
+                Revise what you have learnt with this online quiz.<br/>
+                Click each topic for the different quizes.<br/>
+                Each topic has 10 multiple choice questions.<br/>
+                Remember!<br/>
+                Do not rush through the questions and do your best!
+            </h2>
+        </>
     )
 }
