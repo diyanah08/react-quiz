5fdc5f9892093bed73a0779b29427add91efba06
N Diyanah <diyanah08@gmail.com>
2020-01-04 19:13:50 +0800

    Updates

---
+++ precache-manifest.30c9acd19c805acbe590464ee2d8800e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158064f51f430e3b24304dbefa799486",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "a0e802c1da55f06f3b85",
+    "url": "/react-quiz/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "de2420bd66ac9682f2e3",
+    "url": "/react-quiz/static/css/main.a53e8bd4.chunk.css"
+  },
+  {
+    "revision": "a0e802c1da55f06f3b85",
+    "url": "/react-quiz/static/js/2.6816c927.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-quiz/static/js/2.6816c927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de2420bd66ac9682f2e3",
+    "url": "/react-quiz/static/js/main.eded43c4.chunk.js"
+  },
+  {
+    "revision": "cd8c90277b73c7024e5e",
+    "url": "/react-quiz/static/js/runtime-main.11920cc0.js"
+  },
+  {
+    "revision": "5f39ad2ee1903857f765d849c3a20d59",
+    "url": "/react-quiz/static/media/logo.5f39ad2e.jpg"
+  }
+]);
