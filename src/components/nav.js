16bda6dd3859d95c26c89dc17353c054dd455035
N Diyanah <diyanah08@gmail.com>
2020-01-04 19:26:59 +0800

    edit url links

--- src/components/nav.js
+++ src/components/nav.js
@@ -34,16 +34,16 @@ export default function Nav() {
 
                 <div className="navbar-start">
                     <li className="nav-item">
-                        <Link to='/' className="nav-link">Home</Link>
+                        <Link to='/react-quiz' className="nav-link">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to='/numbers-to-1000' className="nav-link">Numbers to 1000</Link>
+                        <Link to='/react-quiz/numbers-to-1000' className="nav-link">Numbers to 1000</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to='/addition-and-subtraction' className="nav-link">Addition and Subtraction</Link>
+                        <Link to='/react-quiz/addition-and-subtraction' className="nav-link">Addition and Subtraction</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to='/multiplication-and-division' className="nav-link">Multiplication and Division</Link>
+                        <Link to='/react-quiz/multiplication-and-division' className="nav-link">Multiplication and Division</Link>
                     </li>
                 </div>
             </div>
